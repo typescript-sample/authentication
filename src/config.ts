adc7bcd8a4b66e39dd3c8aed4517c1485b567d0c
thanhhaile051020 <thanhhaile0510@gmail.com>
2022-05-27 18:01:46 +0700

    update password

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ export const config = {
   port: 8082,
   secure: false,
   allow: {
-    origin: '*',
+    origin: 'http://localhost:3000',
     credentials: 'true',
     methods: 'GET,PUT,POST,DELETE,OPTIONS,PATCH',
     headers: 'Access-Control-Allow-Headers, Authorization, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
